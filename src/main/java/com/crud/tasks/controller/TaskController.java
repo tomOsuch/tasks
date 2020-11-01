89769e4e8cd68f3ed7b25b964b7c354de9ffe886
tosuch <tomasz.osuch@gmail.com>
2020-11-01 22:24:49 +0100

    task_18_4_TaskController - update

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +18,22 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask() {
         return new TaskDto(1L, "test title", "test_content");
     }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(Long taskId) {
         return new TaskDto(1L, "Edited task title", "Test content");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
 
     }
