ba79b4a9fa7017cd4c36bfe9d37fa46497892729
tosuch <tomasz.osuch@gmail.com>
2020-12-13 01:18:35 +0100

    create swagger doc controller

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -3,13 +3,29 @@ package com.crud.tasks.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/v1/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .build();
     }
 
 }
