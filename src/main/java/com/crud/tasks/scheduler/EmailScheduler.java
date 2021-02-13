4c0f8a87bcfda91e03868600ac7029147ea494e7
tosuch <tomasz.osuch@gmail.com>
2021-02-14 00:46:37 +0100

    main - szablon i wysyka maila

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -17,8 +17,8 @@ public class EmailScheduler {
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
 
-    //@Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
 
         simpleEmailService.send(
